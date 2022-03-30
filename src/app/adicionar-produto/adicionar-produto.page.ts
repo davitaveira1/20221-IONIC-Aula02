072b5bd1f1002de373f2839816f656ff9f3353e8
Davi Taveira <davitaveira@gmail.com>
2022-03-30 20:43:31 -0300

    Repositorio CSS

--- src/app/adicionar-produto/adicionar-produto.page.ts
+++ src/app/adicionar-produto/adicionar-produto.page.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdicionarProdutoPage implements OnInit {
 
+  nome : string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  cadastrar(){
+    
   }
 
 }
