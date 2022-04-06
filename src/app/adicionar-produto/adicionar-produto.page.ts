412ec2299c16329184be3011a45edfa9ea8e4bba
Davi Taveira <davitaveira@gmail.com>
2022-04-06 20:07:02 -0300

    inclusão repositório ts

--- src/app/adicionar-produto/adicionar-produto.page.ts
+++ src/app/adicionar-produto/adicionar-produto.page.ts
@@ -1,3 +1,4 @@
+import { ToastController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -10,13 +11,22 @@ export class AdicionarProdutoPage implements OnInit {
 
   nome : string;
 
-  constructor(private router : Router) { }
+  constructor(private router : Router, private toast : ToastController) { }
 
   ngOnInit() {
   }
 
   cadastrar(){
     this.router.navigate(['selecionar-produto']);
+  }
+
+  async mensagem(msg,duration,color) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: duration,
+      color: color
+    });
+    toast.present();
   }
 
 }
